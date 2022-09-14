2c02288b5245b2edf66715356c5a843b514cf900
opilane <opilane@HP-CZC6297B8L>
2022-09-14 11:36:37 +0300

    Add project files.

---
+++ Kontrolltoo_Mang/Mang.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrolltoo_Mang
+{
+    internal class Mang 
+    {
+        private Tegelane[] characters;
+        public Mang(Tegelane[] characters)
+        {
+            this.characters = characters;
+        }
+
+    }
+}
---
+++ Kontrolltoo_Mang/Peaklass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrolltoo_Mang
+{
+    internal class Peaklass
+    {
+        public static Random rnd = new Random();
+       
+
+        
+        static string getName()
+        {
+            string[] names = { "asd", "fgh", "hjk", "klö", "zxc", "vbn", "nm,", "qwe", "rty", "uio", "püõ" };
+            return names[rnd.Next(names.Length)];
+        }
+
+
+    }
+}
